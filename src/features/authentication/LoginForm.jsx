e098900aedc462fe58d2babca282d2cd3d6c0f28
Federico Montero <federicomontero86@gmail.com>
2024-09-12 16:59:31 -0300

    Adding test credentials to login

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,6 +7,16 @@ import Form from "../../ui/Form";
 import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
 import SpinnerMini from "../../ui/SpinnerMini";
+import styled from "styled-components";
+
+const Label = styled.label`
+  font-weight: 500;
+  font-size: small;
+`;
+const TitleCredentials = styled.label`
+  font-weight: 500;
+  font-size: large;
+`;
 
 function LoginForm() {
   const [email, setEmail] = useState("");
@@ -30,6 +40,7 @@ function LoginForm() {
   return (
     <Form onSubmit={handleSubmit}>
       <FormRowVertical label="Email address">
+        {/* <label>Test Email: federicomontero86@gmail.com</label> */}
         <Input
           type="email"
           id="email"
@@ -49,6 +60,11 @@ function LoginForm() {
           onChange={(e) => setPassword(e.target.value)}
           disabled={isLoading}
         />
+      </FormRowVertical>
+      <FormRowVertical>
+        <TitleCredentials>Test Credentials</TitleCredentials>
+        <Label>Email: xatoca6913@avashost.com</Label>
+        <Label>Password: 12345678</Label>
       </FormRowVertical>
       <FormRowVertical>
         <Button size="large" disabled={isLoading}>
